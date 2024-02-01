18cbde970bdcd40788e44935c3425a7ea08ab32c
Loiane <loianeg@gmail.com>
2024-02-01 15:32:06 -0500

    Add advanced material table component with data binding

--- src/app/advanced-material-table/advanced-material-table.component.ts
+++ src/app/advanced-material-table/advanced-material-table.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
+
+import { ProductService } from './product.service';
 
 @Component({
   selector: 'app-advanced-material-table',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule],
   templateUrl: './advanced-material-table.component.html',
   styleUrls: ['./advanced-material-table.component.scss']
 })
 export class AdvancedMaterialTableComponent {
+
+  productService = inject(ProductService);
+
+  displayedColumns = ['name', 'category', 'price', 'stock', 'created', 'active'];
 
 }
