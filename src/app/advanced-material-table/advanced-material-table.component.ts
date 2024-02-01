84a02a0c6e10d354de1173b8451bca5843481c78
Loiane <loianeg@gmail.com>
2024-02-01 12:47:35 -0500

    created the material table component files

---
+++ src/app/advanced-material-table/advanced-material-table.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-advanced-material-table',
+  standalone: true,
+  imports: [],
+  templateUrl: './advanced-material-table.component.html',
+  styleUrls: ['./advanced-material-table.component.scss']
+})
+export class AdvancedMaterialTableComponent {
+
+}
